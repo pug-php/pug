8a50e6fedca1167733b3128a8eea42d9f33e797d
Nick Sinopoli <NSinopoli@gmail.com>
2011-06-19 15:56:24 -0700

    Cleanup.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,15 +1,39 @@
 <?php
 
-/*
- * This file is part of the HTAML package.
- * (c) 2010 Konstantin Kudryashov <ever.zet@gmail.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+define('JP_ROOT', dirname(__DIR__));
 
-if (is_file(__DIR__.'/../autoload.php')) {
-  require_once __DIR__ . '/../autoload.php';
-} else {
-  require_once __DIR__ . '/../autoload.php.dist';
+set_include_path(
+    get_include_path()  . PATH_SEPARATOR .
+    JP_ROOT             . PATH_SEPARATOR
+);
+
+function file_exists_in_include_path($file) {
+    if ( file_exists($file) ) {
+        return realpath($file);
+    }
+
+    $paths = explode(PATH_SEPARATOR, get_include_path());
+
+    foreach ( $paths as $path ) {
+        $fullpath = $path . DIRECTORY_SEPARATOR . $file;
+
+        if ( file_exists($fullpath) ) {
+            return realpath($fullpath);
+        }
+    }
+
+    return false;
 }
+
+function autoload($class) {
+    $file = str_replace("\\", "/", $class) . '.php';
+    if ( file_exists_in_include_path($file) ) {
+        require_once $file;
+    } else {
+        // TODO: Throw exception!
+    }
+}
+
+spl_autoload_register('autoload');
+
+?>
