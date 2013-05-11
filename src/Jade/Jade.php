9273fdbc74d95263b7820c7dd4446ea70e545b49
Ronan <tessierronan@yahoo.fr>
2013-05-11 15:38:03 +0200

    Creates stream for non-cached render

--- src/Jade/Jade.php
+++ src/Jade/Jade.php
@@ -14,17 +14,30 @@ class Jade {
     /**
      * @var bool
      */
-    protected $prettyprint = false;
+    protected $prettyPrint = false;
     /**
      * @var null
      */
     protected $cachePath = null;
 
     /**
+     * @var string
+     */
+    protected $wrapperName = 'jade.stream';
+
+    /**
+     * Indicate if we registed the stream wrapper,
+     * in order to not ask the stream registry each time
+     * We need to render a template
+     * @var bool
+     */
+    protected static $isWrapperRegistered = false;
+
+    /**
      * @param array $options
      */
-    public function __construct(array $options = array()) {
-
+    public function __construct(array $options = array())
+    {
         foreach ($options as $key => $opt)
         {
             if (property_exists($this, $key))
@@ -36,65 +49,79 @@ class Jade {
 
     /**
      * @param $input
-     * @param array $scope
      * @return string
      */
-    public function compile($input, array $scope = array()) {
-
-        extract($scope);
-
+    public function compile($input)
+    {
         $parser     = new Parser($input);
-        $compiler   = new Compiler($this->prettyprint);
+        $compiler   = new Compiler($this->prettyPrint);
 
-        $output = $compiler->compile($parser->parse($input));
-
-        return $output;
+        return $compiler->compile($parser->parse($input));
     }
 
     /**
      * @param $input
-     * @param array $scope
+     * @param array $vars
      * @return mixed|string
      */
-    public function render($input, array $scope = array())
+    public function render($input, array $vars = array())
     {
-        return $this->cachePath
-            ? $this->cache($input, $scope)
-            : $this->compile($input, $scope);
+        $file = $this->cachePath ? $this->cache($input) : $this->stream($input);
+
+        extract($vars);
+        return include $file;
     }
 
     /**
+     * Create a stream wrapper to allow
+     * the possibility to add $scope variables
      * @param $input
-     * @param array $scope
+     * @return string
+     */
+    public function stream($input)
+    {
+        if (! static::$isWrapperRegistered)
+        {
+            static::$isWrapperRegistered = true;
+            stream_wrapper_register($this->wrapperName, 'Jade\Stream');
+        }
+        return $this->wrapperName.'://data;'.base64_encode($input);
+    }
+
+
+    /**
+     * @param $input
      * @return mixed|string
      * @throws \InvalidArgumentException
      * @throws \Exception
      */
-    public function cache($input, array $scope = array()) {
-
-        if ( $this->cachePath == null || ! is_dir($this->cachePath) ) {
+    public function cache($input)
+    {
+        if (! is_file($input))
+        {
+            throw new \InvalidArgumentException('Only files can be cached.');
+        }
+        if ($this->cachePath == null || ! is_dir($this->cachePath) )
+        {
             throw new \Exception('You must provide correct cache path to Jade for caching.');
         }
-        if ( !is_writable($this->cachePath) ) {
+
+        $path = str_replace('//', '/', $this->cachePath . '/' . md5($input) . '.php');
+        $cacheTime = ! file_exists($path) ? 0 : filemtime($path);
+
+        // Do not re-parse file if original is older
+        if ( $cacheTime && filemtime($input) < $cacheTime )
+        {
+            return $path;
+        }
+        if (! is_writable($this->cachePath) )
+        {
             throw new \Exception(sprintf('Cache directory must be writable. "%s" is not.', $this->cachePath));
         }
-        if ( !is_file($input) ) {
-            throw new \InvalidArgumentException('Only file templates can be cached.');
-        }
 
-        $cacheKey = 'jade-' . sha1($input);
-        $path = $this->cachePath . '/' . $cacheKey . '.php';
-
-        $cacheTime = file_exists($path) ? 0 : filemtime($path);
-
-        if ( $cacheTime && filemtime($input) < $cacheTime ) {
-            extract($scope);
-            return include $path;
-        }
-
-        $rendered = $this->compile($input, $scope);
+        $rendered = $this->compile($input);
         file_put_contents($path, $rendered);
 
-        return $rendered;
+        return $this->stream($rendered);
     }
 }
