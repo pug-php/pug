ff22a2421eb5f0583040a48c47179a57d21e707f
Ronan <tessierronan@yahoo.fr>
2013-05-11 14:23:42 +0200

    Move folders

---
+++ src/Jade/Filter/Cdata.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @Author      ronan.tessier@vaconsulting.lu
+ * @Date        11/05/13
+ * @File        Cdata.php
+ * @Copyright   Copyright (c) jadephp - All rights reserved
+ * @Licence     Unauthorized copying of this source code, via any medium is strictly
+ *              prohibited, proprietary and confidential.
+ */
+
+namespace Jade\Filter;
+
+
+class Cdata {
+
+}
---
+++ src/Jade/Filter/FilterInterface.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @Author      ronan.tessier@vaconsulting.lu
+ * @Date        11/05/13
+ * @File        FilterInterface.php
+ * @Copyright   Copyright (c) jadephp - All rights reserved
+ * @Licence     Unauthorized copying of this source code, via any medium is strictly
+ *              prohibited, proprietary and confidential.
+ */
+
+namespace Jade\Nodes;
+
+interface FilterInterface {
+
+    public function getData()
+
+}
