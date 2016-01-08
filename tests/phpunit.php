c8326cac90b09d1bc02d8c63deb0fed151256317
volter9 <volter925@gmail.com>
2016-01-08 12:06:50 +0000

    Pick @volter9 Progression for PHPUnit tests

--- tests/phpunit.php
+++ tests/phpunit.php
@@ -15,20 +15,27 @@ class JadePHPTest extends PHPUnit_Framework_TestCase {
     );
 
     public function caseProvider() {
-        static $rawResults = null;
-        if(is_null($rawResults)) {
-            $rawResults = get_tests_results();
-            $rawResults = $rawResults['results'];
+        $array = [];
+
+        foreach (build_list(find_tests()) as $arr) {
+            foreach ($arr as $e) {
+                if ($e['name'] === 'index') {
+                    continue;
+                }
+
+                $array[] = [$e['name']];
+            }
         }
-        return $rawResults;
+
+        return $array;
     }
 
     /**
      * @dataProvider caseProvider
      */
-    public function testStringGeneration($name, $input, $expected) {
-        if(! in_array($name, static::$skipped)) {
-            $this->assertSame($input, $expected, $name);
-        }
+    public function testJadeGeneration($name) {
+        $result = get_test_result($name)[1];
+
+        $this->assertSame($result[1], $result[2], $name);
     }
 }
