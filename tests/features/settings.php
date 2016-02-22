b638180b49ea1132187ad8bf424629f99a06ff03
kylekatarnls <kylekatarnls@gmail.com>
2016-02-22 13:04:22 +0100

    Add approximative prettyprint unit test

--- tests/features/settings.php
+++ tests/features/settings.php
@@ -6,8 +6,9 @@ class JadeSettingsTest extends PHPUnit_Framework_TestCase {
 
     static private function rawHtml($html) {
 
-        return trim(preg_replace('`\n+`', "\n", strtr(str_replace(array("\r", ' '), '', $html), "'", '"')));
+        return trim(preg_replace('`\n{2,}`', "\n", strtr(str_replace(array("\r", ' '), '', $html), "'", '"')));
     }
+
     /**
      * keepNullAttributes setting test
      */
@@ -21,7 +22,7 @@ class JadeSettingsTest extends PHPUnit_Framework_TestCase {
         $actual = $jade->render(file_get_contents($templates . 'mixin.attrs.jade'));
         $expected = file_get_contents($templates . 'mixin.attrs.html');
 
-        $this->assertSame(static::rawHtml($actual), static::rawHtml($expected), 'Keep null attributes');
+        $this->assertSame(static::rawHtml($actual), static::rawHtml($expected), 'Keep null attributes disabled');
 
         $jade = new Jade(array(
             'keepNullAttributes' => true,
@@ -31,6 +32,51 @@ class JadeSettingsTest extends PHPUnit_Framework_TestCase {
         $actual = $jade->render(file_get_contents($templates . 'mixin.attrs.jade'));
         $expected = file_get_contents($templates . 'mixin.attrs-keep-null-attributes.html');
 
-        $this->assertSame(static::rawHtml($actual), static::rawHtml($expected), 'Keep null attributes');
+        $this->assertSame(static::rawHtml($actual), static::rawHtml($expected), 'Keep null attributes enabled');
+    }
+
+    /**
+     * prettyprint setting test
+     */
+    public function testPrettyprint() {
+
+        $template = '
+mixin centered(title)
+  div.centered(id=attributes.id)
+    - if (title)
+      h1(class=attributes.class)= title
+    block
+    - if (attributes.href)
+      .footer
+        a(href=attributes.href) Back
+
++centered(\'Section 1\')#Second
+  p Some important content.
+';
+
+        $jade = new Jade(array(
+            'prettyprint' => true,
+        ));
+        $actual = trim(preg_replace('`[ \t]+`', ' ', preg_replace('`\n( +\n)+`', "\n", str_replace("\r", '', $jade->render($template)))));
+        $expected = trim('
+ <div id=\'Second\' class=\'centered\'>
+ <h1 >
+ Section 1 ' . '
+ </h1>
+<p>
+ Some important content.
+</p>
+ </div>
+');
+
+        $this->assertSame($actual, $expected, 'Pretty print enabled');
+
+        $jade = new Jade(array(
+            'prettyprint' => false,
+        ));
+        $actual = preg_replace('`(<[a-z][a-z:_0-9]*) (?=[\s>])`', '$1', $jade->render($template));
+        $expected = '<div id=\'Second\' class=\'centered\'><h1>Section 1</h1><p>Some important content.</p></div>';
+
+        $this->assertSame($actual, $expected, 'Pretty print disabled');
     }
 }
