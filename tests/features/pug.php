4ba08632bd77d9371a6553bc31c5817c7b56debf
kylekatarnls <kylekatarnls@gmail.com>
2016-05-18 11:42:54 +0200

    Code coverage

--- tests/features/pug.php
+++ tests/features/pug.php
@@ -16,5 +16,17 @@ class PugAliasTest extends PHPUnit_Framework_TestCase {
         $this->assertSame($jade->getOption('stream'), 'jade.stream');
         $this->assertSame($pug->getOption('stream'), 'pug.stream');
         $this->assertSame($pug->render('p Hello'), '<p>Hello</p>');
+        $this->assertSame($pug->getExtension(), '.pug');
+
+        $jade = new Jade(array(
+            'extension' => '.foo',
+        ));
+        $this->assertSame($jade->getExtension(), '.foo');
+
+        $jade->setOption('extension', array('.jade', '.pug'));
+        $this->assertSame($jade->getExtension(), '.jade');
+
+        $jade->setOption('extension', array());
+        $this->assertSame($jade->getExtension(), '');
     }
 }
