e77b0e3954ecaecc515a6f416df7fa98f2b17b1b
Debo <debo@users.noreply.github.com>
2016-10-23 21:58:33 +1100

    Fix examples to support current implementation (#79)

--- example/bootstrap.php
+++ example/bootstrap.php
@@ -1,31 +1,37 @@
 <?php
 namespace Pug;
 
-error_reporting(E_ALL);
+error_reporting(E_ALL & ~E_NOTICE);
 ini_set('display_errors', 1);
 
-class Application {
+class Application
+{
     protected $route;
+
     public function __construct($srcPath, $pathInfo)
     {
         $this->route = isset($pathInfo) ? ltrim($pathInfo, '/') : '/';
 
-        spl_autoload_register(function($class) use($srcPath) {
+        spl_autoload_register(function ($class) use ($srcPath) {
             if (
                 strstr($class, 'Pug') /* new name */ ||
                 strstr($class, 'Jade') /* old name */
             ) {
-            	include($srcPath . str_replace("\\", DIRECTORY_SEPARATOR, $class) . '.php');
+                include($srcPath . str_replace("\\", DIRECTORY_SEPARATOR, $class) . '.php');
             }
         });
     }
+
     public function action($path, \Closure $callback)
     {
         if ($path === $this->route) {
-            $pug = new Pug;
-            $vars = $callback($path) ?: [];
-            $pug->render($path . $pug->getExtension(), $vars);
+            $pug    = new Pug;
+            $vars   = $callback($path) ?: [];
+            $output = $pug->render($path . $pug->getExtension(), $vars);
+
+            echo $output;
         }
     }
 }
+
 $app = new Application('../src/', $_SERVER['PATH_INFO']);
--- example/index.php
+++ example/index.php
@@ -5,48 +5,45 @@ require 'bootstrap.php';
 /**
  * Home pages list
  */
-$app->action('/', function(&$view)
-{
+$app->action('/', function (&$view) {
     $view  = 'index';
     $items = array(
-        array('route' => 'login',  'name' => 'The Login page example'),
+        array('route' => 'login', 'name' => 'The Login page example'),
         array('route' => 'cities', 'name' => 'The City API search example'),
     );
+
     return compact('items');
 });
 
 /**
  * Basic user form
  */
-$app->action('login', function()
-{
+$app->action('login', function () {
     $username = isset($_POST['username']) ? $_POST['username'] : null;
     $password = isset($_POST['password']) ? $_POST['password'] : null;
 
-    $message['error']   = !! $_POST;
+    $message['error']   = !!$_POST;
     $message['success'] = false;
 
-    if (trim($username) && trim($password)){
+    if (trim($username) && trim($password)) {
         $message['error']   = false;
         $message['success'] = true;
     }
 
     return array(
-        'message'=> $message,
-        'user'   => compact('username', 'password', 'invalid')
+        'message' => $message,
+        'user'    => compact('username', 'password', 'invalid')
     );
 });
 
 /**
  * Cities list search
  */
-$app->action('cities', function()
-{
+$app->action('cities', function () {
     $query  = isset($_GET['q']) ? trim($_GET['q']) : null;
     $cities = ['total' => 0, 'result' => null];
 
-    if ($query)
-    {
+    if ($query) {
         $ch = curl_init('http://gd.geobytes.com/AutoCompleteCity?q=' . $query);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $cities['result'] = json_decode(curl_exec($ch));
