0478c43b29879ae529ed9a9dd236617134383241
KyleKatarn <kylekatarnls@gmail.com>
2017-05-04 16:01:53 +0200

    Debug Travis error

--- tests/features/pugjs.php
+++ tests/features/pugjs.php
@@ -30,6 +30,10 @@ class PugJsTest extends PHPUnit_Framework_TestCase
         clearstatcache();
         $html = trim($pug->render($source));
 
+        if (!file_exists($cache)) {
+            var_dump($html);
+            exit(1);
+        }
         $this->assertTrue(file_exists($cache));
 
         $this->assertSame('<html><body><h1>Title</h1></body></html>', $html);
