c040ec8261bebcf32c144012d2d16fedb54527ea
KyleKatarn <kylekatarnls@gmail.com>
2017-05-15 00:35:22 +0200

    Reverse Pug and Jade Filter interface to ease extends for filters

--- src/Pug/Filter/FilterInterface.php
+++ src/Pug/Filter/FilterInterface.php
@@ -2,9 +2,13 @@
 
 namespace Pug\Filter;
 
+use Jade\Compiler;
+use Jade\Nodes\Filter;
+
 /**
  * Interface Pug\Filter\FilterInterface.
  */
-interface FilterInterface extends \Jade\Filter\FilterInterface
+interface FilterInterface
 {
+    public function __invoke(Filter $filter, Compiler $compiler);
 }
